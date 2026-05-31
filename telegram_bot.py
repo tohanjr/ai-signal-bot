2402c12649bd00ffc40fd20c1a8f724f4d62aec1
tohanjr <tohan765@gmail.com>
2026-05-31 23:26:43 +0600

    telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,16 @@
 from telegram import Bot
+import asyncio
+
 from config import TELEGRAM_TOKEN, CHANNEL_ID
 
 bot = Bot(token=TELEGRAM_TOKEN)
 
 def send_signal(message):
 
-    bot.send_message(
-        chat_id=CHANNEL_ID,
-        text=message
-    )
+    async def send():
+        await bot.send_message(
+            chat_id=CHANNEL_ID,
+            text=message
+        )
+
+    asyncio.run(send())
